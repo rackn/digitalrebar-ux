ef7d9e58ae7da93ab7257d0688e33015d1a3026a
Rob Hirschfeld <rob@rackn.com>
2016-01-03 20:26:44 -0600

    allow user to over-rride default port

--- simple-https.py
+++ simple-https.py
@@ -1,3 +1,4 @@
+# Copyright RackN, 2015
 # generate server.pem with the following command:
 #    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
 # run as follows:
@@ -6,8 +7,11 @@
 #    https://localhost:443
 
 import BaseHTTPServer, SimpleHTTPServer
-import ssl
+import ssl, sys
 
-httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+port = len(sys.argv) == 2 and int(sys.argv[1]) or 443
+print "Starting US Server on port %d (pass arg to override)" % (port)
+
+httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
 httpd.serve_forever()
