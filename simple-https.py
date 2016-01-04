7ae7c5bb6d850286d273126cd982f5bee474de18
Rob Hirschfeld <rob@rackn.com>
2016-01-03 20:31:27 -0600

    update readme on new param

--- simple-https.py
+++ simple-https.py
@@ -10,7 +10,7 @@ import BaseHTTPServer, SimpleHTTPServer
 import ssl, sys
 
 port = len(sys.argv) == 2 and int(sys.argv[1]) or 443
-print "Starting US Server on port %d (pass arg to override)" % (port)
+print "Starting UX Server on port %d (pass arg to override)" % (port)
 
 httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
