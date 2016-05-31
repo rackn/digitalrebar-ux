7e54ee6203f44558c698542296c64c7ef062044a
meshiest <meek.mesh@gmail.com>
2016-05-31 15:36:49 -0500

    network selection and delete with confirmation

--- js/networks.js
+++ js/networks.js
@@ -3,7 +3,7 @@ Copyright 2015, RackN
 network controller
 */
 (function(){
-    angular.module('app').controller('NetworkCtrl', function($scope) {
+    angular.module('app').controller('NetworkCtrl', function($scope, api, $mdDialog, $mdMedia) {
         $scope.$emit('title', 'Networks'); // shows up on the top toolbar
 
         var networks = this;
@@ -16,6 +16,28 @@ network controller
         		networks.push($scope._networks[id])
         	}
         	return networks
+        }
+
+        this.deleteSelected = function(event) {
+            $scope.confirm(event, {
+                title: "Remove Networks",
+                message: "Are you sure you want to remove selected networks?",
+                yesCallback: function(){
+                    networks.selected.forEach(function(network) {
+
+                        // the api call uses REST DELETE on /nodes/id to remove a node 
+                        api('/api/v2/networks/'+network.id, {method: 'DELETE'}).
+                            success(function(){
+                                console.log("network deleted")
+                            }).success(function(){
+                                api.remove("network", network.id)
+                            })
+                    })
+
+                    // remove the selected items
+                    networks.selected = []
+                }
+            })
         }
 
     });
