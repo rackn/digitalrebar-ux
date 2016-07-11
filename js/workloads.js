bb8a19d642fdf7d69c3265d00b1f6c756aa34895
meshiest <meek.mesh@gmail.com>
2016-07-11 12:28:47 -0500

    moved towards server-side workloads and local json blobs

--- js/workloads.js
+++ js/workloads.js
@@ -239,179 +239,67 @@ workloads controller
         return nodes.concat($scope.createdNodes);
       };
 
-      $scope.wizardStatus = 0;
-      $scope.startWizard = function () {
-        console.log("Starting wizard")
-        if (!$scope.overallStatus())
-          return;
-
-        $scope.wizardStatus = 1;
-        /* create deployment
-         * move nodes
-         * create nodes
-         * bind nodes
-         */
-        var deployment_id = -1;
-
-        var createDeployment = function () {
-          console.log("creating deployments")
-          api('/api/v2/deployments', {
-            method: "POST",
-            data: {
-              name: workloads.name
-            }
-          }).success(api.addDeployment).success(function (data) {
-            deployment_id = data.id;
-            $scope.wizardStatus = 2;
-            moveNodes();
-          }).error(function(e){console.log("failed on createDeployment",e)});
+      $scope.generateBlob = function () {
+        var data = {
+          name: workloads.name,
+          os: workloads.os
         };
 
-        // add 'rebar-installed-node' role if node provider type is metalprovider
-        // set metal node attrib: 'provisioner-target_os' to metal os
-
-        var moveNodes = function () {
-          console.log("Moving nodes")
-
-          var realNodes = []
-          for (var i in workloads.selected) {
-            var node = workloads.selected[i];
-            if (node.id > 0) {
-              realNodes.push(node);
-              console.log('adding',node.id)
-            }
-          }
-          var count = realNodes.length;
-          console.log('moving',count,'nodes')
-          var part = 1 / count;
-          realNodes.forEach(function (node) {
-            api("/api/v2/nodes/" + node.id, {
-              method: "PUT",
-              data: {
-                node_deployment: {
-                  deployment_id: deployment_id,
-                  old_deployment: node.deployment_id
-                }
-              }
-            }).success(api.addNode).success(function () {
-              $scope.wizardStatus += part;
-              count--;
-              if (count == 0) {
-                $scope.wizardStatus = 3;
-                createNodes();
-              }
-            }).error(function(e){console.log("failed on moveNode",e)});
-          });
-          if(count == 0) {
-            console.log('no nodes to move, creating nodes')
-            $scope.wizardStatus = 3;
-            createNodes();
-          }
-        };
-
-        var createNodes = function () {
-          console.log("Creating Nodes")
-          for (var i in $scope.createdNodes) {
-            $scope.createdNodes[i].index = i;
-          }
-          var count = $scope.createdNodes.length;
-          var part = 1 / count;
-          $scope.createdNodes.forEach(function (node) {
-            var i = node.index;
-            var payload = {
-              'name': workloads.name + "-" + i + "." + workloads.provider + ".neode.org",
-              'description': "created by rebar",
-              'provider': workloads.provider,
-              'deployment_id': deployment_id,
-              'hints': {
-                'use-proxy': false,
-                'use-ntp': false,
-                'use-dns': false,
-                'use-logging': false,
-                'provider-create-hint': {
-                  'hostname': workloads.name + '-' + i
-                }
-              }
-            };
-            if (workloads.add_os != "default_os") {
-              // packet
-              payload.hints['provider-create-hint'].os = workloads.add_os;
-              // aws
-              payload.hints['provider-create-hint'].image_id = workloads.add_os;
-              // google
-              payload.hints['provider-create-hint'].disks = [];
-              payload.hints['provider-create-hint'].disks.push({
-                'autoDelete': true,
-                'boot': true,
-                'type': 'PERSISTENT',
-                'initializeParams': {
-                  'sourceImage': workloads.add_os
-                }
-              });
-            };
-
-
-            api('/api/v2/nodes', {
-              method: "POST",
-              data: payload,
-            }).success(api.addNode).success(function (data) {
-              node.real_node = data.id;
-              $scope.wizardStatus += part;
-              count--;
-              if (count == 0) {
-                $scope.wizardStatus = 4;
-                bindNodes();
-              }
-            }).error(function(e){console.log("failed on createNodes",e)});
-          });
-          if(count == 0) {
-            console.log('no nodes to create, binding')
-            $scope.wizardStatus = 4;
-            createNodes();
-          }
-        };
-
-        var bindNodes = function () {
-          console.log("Binding Nodes")
-          var count = 0;
-          for (var i in serviceMap) {
-            for (var j in serviceMap[i]) {
-              if (serviceMap[i][j])
-                count++;
-            }
-          }
-          var part = 1 / count;
-          workloads.selected.forEach(function (node) {
-            var node_id = node.id < 0 ? node.real_node : node.id;
-            for (var roleName in serviceMap[node.id]) {
-              if (serviceMap[node.id][roleName]) {
-                var role_id = $scope.roleMap[roleName].id;
-                api("/api/v2/node_roles/", {
-                  method: "POST",
-                  data: {
-                    node_id: node_id,
-                    deployment_id: deployment_id,
-                    role_id: role_id
-                  }
-                }).success(api.addNodeRole).success(function () {
-                  $scope.wizardStatus += part;
-                  count--;
-                  if (count == 0) {
-                    $scope.wizardStatus = 5;
-                    finishWizard();
-                  }
-                }).error(function(e){console.log("failed on bindNodes",e)});
-              }
-            }
-          });
-        };
-
-        var finishWizard = function () {
-          console.log("Done!");
+        if (wizard.create_nodes) {
+          data.provider = {
+            name: provider.name,
+            os: workloads.add_os
+          };
         }
 
-        createDeployment();
+        var virtualNodes = 0;
+        data.nodes = [];
 
+        for (var i in workloads.selected) {
+          var node = workloads.selected[i];
+          var id = node.id;
+          var roles = [];
+
+          for (var role in serviceMap[id]) {
+            if (serviceMap[id][role])
+              roles.push($scope.roleMap[role].id);
+          }
+
+          roles = roles.sort();
+
+          if (id > 0) {
+            data.nodes.push({
+              node_id: id,
+              roles: roles
+            });
+          } else {
+            var existing = false;
+            // check to see if we already have a cloud node with the same roles
+            for (var j in data.nodes) {
+              var n = data.nodes[j];
+              if (n.id > 0)
+                continue;
+              // unfortunately the only way to compare arrays
+              if (JSON.stringify(n.roles) == JSON.stringify(roles)) {
+                n.node_count++;
+                existing = true;
+                break;
+              }
+            }
+
+            if (!existing) {
+              console.log("Adding virtual node",virtualNodes+1)
+              data.nodes.push({
+                node_id: -(++virtualNodes),
+                roles: roles,
+                node_count: 1
+              });
+            }
+          }
+
+        }
+
+        console.log(JSON.stringify(data, null, "  "))
       };
 
     });
