9aeba3c0201da0daabda1cbe13b9630a26cc616e
Rob @zehicle Hirschfeld <rob@rackn.com>
2016-09-21 14:22:28 -0500

    read only capabilities view

--- js/capabilities.js
+++ js/capabilities.js
@@ -6,6 +6,16 @@ capabilities controller
     .controller('CapabilitiesCtrl', function ($scope, api, $mdMedia, $mdDialog, $routeParams) {
       $scope.$emit('title', 'Capabilities'); // shows up on the top toolbar
 
-  });
-    
+    })
+    .filter('groupsonly', function(){
+      return function(cap) {
+        filter = []
+        for (var i in cap) {
+          name = cap[i].name;
+          if (cap[i].includes.length > 0 && name.substr(name.length - 4) != "_ALL")
+            filter.push(cap[i]);
+        }
+        return filter;
+      }
+    });
 })();
