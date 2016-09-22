1d9d710ea5b723405296be9158cdc9a5b24886bd
Rob @zehicle Hirschfeld <rob@rackn.com>
2016-09-21 22:19:30 -0500

    working capabilities editor  - requires core change to add dr-groups capabilities

--- js/capabilities.js
+++ js/capabilities.js
@@ -6,13 +6,49 @@ capabilities controller
     .controller('CapabilitiesCtrl', function ($scope, api, $mdMedia, $mdDialog, $routeParams) {
       $scope.$emit('title', 'Capabilities'); // shows up on the top toolbar
 
+      $scope.updateCapability = function(cap) {
+        console.debug({ 'includes': cap.includes });
+        api('/api/v2/capabilities/'+ cap.id, {
+          method: 'PUT',
+          data: { 'includes': cap.includes }
+        }).success(function (update) {
+          api.getHealth();
+        }).error(function (err) {
+          api.getHealth();
+        });
+      };
+
+      $scope.deleteCapability = function(cap) {
+        $scope.confirm(event, {
+          title: "Delete Capability",
+          message: "Are you sure you want to delete selected capability?",
+          yesCallback: function () {
+            api('/api/v2/capabilities/' + cap.id, {
+              method: 'DELETE'
+            }).error(function (err) {
+              api.toast('Error Deleted Capability', 'capability', err);
+            }).success(function () {
+              api.toast('Deleted ' + capability + ' capability');
+            })
+          }
+        });
+      };
+
+      $scope.rawCapabilities = function(current) {
+        raw = [];
+        for (var i in $scope._capabilities) {
+          if (!current.includes($scope._capabilities[i].name))
+            raw.push($scope._capabilities[i].name);
+        }
+        return raw;
+      };
+
     })
     .filter('groupsonly', function(){
       return function(cap) {
-        filter = []
+        filter = [];
         for (var i in cap) {
-          name = cap[i].name;
-          if (cap[i].includes.length > 0 && name.substr(name.length - 4) != "_ALL")
+          if (cap[i].source === "dr-groups")
             filter.push(cap[i]);
         }
         return filter;
