a86ed1ef3e88be5bd934384334296ae42de883ae
Meshiest <meek.mesh@gmail.com>
2017-06-05 14:23:03 -0500

    removed debug

--- app/js/workloads.js
+++ app/js/workloads.js
@@ -450,10 +450,8 @@ workloads controller
 
       $scope.getNodes = function () {
         if (workloads.use_system) {
-          console.log('system now?')
           workloads.selected = $scope.systemNodes;
         } else {
-          console.log('new nodes');
 
           workloads.selected = $scope.createdNodes;
         }
