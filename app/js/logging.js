23e76261f2a897662afbbe53dc2f49c415cd1e7f
Meshiest <meek.mesh@gmail.com>
2017-06-07 14:14:23 -0500

    Change backtick html to single quote html

--- app/js/logging.js
+++ app/js/logging.js
@@ -16,22 +16,22 @@
 
         $scope.showInfo = function (ev, err) {
            $mdDialog.show({
-            template: `
-              <md-dialog aria-label="Error">
-                <md-toolbar>
-                  <div class="md-toolbar-tools">
-                    <h2>` + err.type + ' - ' + err.message + `</h2>
-                  </div>
-                </md-toolbar>
-                <md-dialog-content>
-                  <div class="md-dialog-content">
-                    <b>Error:&nbsp;</b>
-                    <pre>` + JSON.stringify(err.err, null, '  ') + `</pre>
-                    <b>Stack:</b>
-                    <pre>` + err.stack + `</pre>
-                  </div>
-                </md-dialog-content>
-              </md-dialog>`,
+            template: '' +
+              '<md-dialog aria-label="Error">' +
+              '  <md-toolbar>' +
+              '    <div class="md-toolbar-tools">' +
+              '      <h2>' + err.type + ' - ' + err.message + '</h2>' +
+              '    </div>' +
+              '  </md-toolbar>' +
+              '  <md-dialog-content>' +
+              '    <div class="md-dialog-content">' +
+              '      <b>Error:&nbsp;</b>' +
+              '      <pre>' + JSON.stringify(err.err, null, '  ') + '</pre>' +
+              '      <b>Stack:</b>' +
+              '      <pre>' + err.stack + '</pre>' +
+              '    </div>' +
+              '  </md-dialog-content>' +
+              '</md-dialog>',
             parent: angular.element(document.body),
             targetEvent: ev,
             clickOutsideToClose: true
