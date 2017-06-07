7a463329c32a8557bf53591b1336d07052e3024d
Meshiest <meek.mesh@gmail.com>
2017-06-07 13:57:25 -0500

    Fix whitespace and structure of welcome

--- app/js/welcome.js
+++ app/js/welcome.js
@@ -1,11 +1,13 @@
 /*
-welcome controller
+  Copyright 2017, RackN
+  Welcome Controller
 */
 (function () {
   angular.module('app')
-    .controller('WelcomeCtrl', function ($scope, api, $location, localStorageService, $http, $cookies, debounce, $mdMedia, $mdDialog, $mdToast) {
+  .controller('WelcomeCtrl', [
+    '$scope',
+    function ($scope) {
       $scope.$emit('title', 'Welcome to RackN Digital Rebar!'); // shows up on the top toolbar
-
-
-    });
+    }
+  ]);
 })();
