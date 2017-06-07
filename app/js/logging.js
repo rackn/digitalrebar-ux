541eee251fcc24be81c6e4afaaaff66e8eca274f
Meshiest <meek.mesh@gmail.com>
2017-06-07 11:46:52 -0500

    Fix whitespace and structure of logging

--- app/js/logging.js
+++ app/js/logging.js
@@ -1,73 +1,77 @@
 /*
-logging controller
+  Copyright 2017, RackN
+  Logging Controller
 */
 (function () {
   angular.module('app')
-    .controller('LoggingCtrl', function ($scope, api, $mdDialog, $mdMedia, localStorageService) {
-      $scope.$emit('title', 'Alert History'); // shows up on the top toolbar
+    .controller('LoggingCtrl', [
+      '$scope', 'api', '$mdDialog', '$mdMedia', 'localStorageService',
+      function ($scope, api, $mdDialog, $mdMedia, localStorageService) {
+        $scope.$emit('title', 'Alert History'); // shows up on the top toolbar
 
-      $scope.query = {
-        page: 1,
-        limit: 10,
-      };
+        $scope.query = {
+          page: 1,
+          limit: 10,
+        };
 
-      $scope.showInfo = function (ev, err) {
-         $mdDialog.show({
-          template: 
-            '<md-dialog aria-label="Error">' +
-            '  <md-toolbar>' +
-            '    <div class="md-toolbar-tools">' +
-            '      <h2>' + err.type + " - " + err.message + '</h2>' +
-            '    </div>' +
-            '  </md-toolbar>' +
-            '  <md-dialog-content>' +
-            '    <div class="md-dialog-content">' +
-            '      <b>Error:&nbsp;</b>' +
-            '      <pre>' + JSON.stringify(err.err, null, "  ") + '</pre>' +
-            '      <b>Stack:</b>' +
-            '      <pre>' + err.stack + '</pre>' +
-            '    </div>' +
-            '  </md-dialog-content>' +
-            '</md-dialog>',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          clickOutsideToClose: true
-        });
-      };
+        $scope.showInfo = function (ev, err) {
+           $mdDialog.show({
+            template: `
+              <md-dialog aria-label="Error">
+                <md-toolbar>
+                  <div class="md-toolbar-tools">
+                    <h2>` + err.type + ' - ' + err.message + `</h2>
+                  </div>
+                </md-toolbar>
+                <md-dialog-content>
+                  <div class="md-dialog-content">
+                    <b>Error:&nbsp;</b>
+                    <pre>` + JSON.stringify(err.err, null, '  ') + `</pre>
+                    <b>Stack:</b>
+                    <pre>` + err.stack + `</pre>
+                  </div>
+                </md-dialog-content>
+              </md-dialog>`,
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true
+          });
+        };
 
-      $scope.remove = function (err) {
-        api.errors.splice(api.errors.indexOf(err), 1);
-        localStorageService.add('errors', api.errors);
-      };
+        $scope.remove = function (err) {
+          api.errors.splice(api.errors.indexOf(err), 1);
+          localStorageService.add('errors', api.errors);
+        };
 
-      $scope.method = 'get';
-      $scope.payload = '{}';
-      $scope.route = '';
-      $scope.class = {};
-
-      $scope.parse = function (data) {
-        try {
-          var a = JSON.parse(data);
-          return a;
-        } catch (e) {
-          return false;
-        }
-      };
-
-      $scope.testApi = function () {
-        api($scope.route, {method: $scope.method, data: JSON.parse($scope.payload)}).
-        then(function (resp) {
-          $scope.class = {success: true};
-          $scope.output = JSON.stringify(resp.data, null, "  ");
-        }, function (err) {
-          $scope.class = {error: true};
-          $scope.output = JSON.stringify(err && err.data || err, null, "  ") || err.data;
-        });
-      };
-
-      $scope.clearOutput = function () {
-        $scope.output = "";
+        $scope.method = 'get';
+        $scope.payload = '{}';
+        $scope.route = '';
         $scope.class = {};
+
+        $scope.parse = function (data) {
+          try {
+            var a = JSON.parse(data);
+            return a;
+          } catch (e) {
+            return false;
+          }
+        };
+
+        $scope.testApi = function () {
+          api($scope.route, {method: $scope.method, data: JSON.parse($scope.payload)}).
+          then(function (resp) {
+            $scope.class = {success: true};
+            $scope.output = JSON.stringify(resp.data, null, '  ');
+          }, function (err) {
+            $scope.class = {error: true};
+            $scope.output = JSON.stringify(err && err.data || err, null, '  ') || err.data;
+          });
+        };
+
+        $scope.clearOutput = function () {
+          $scope.output = '';
+          $scope.class = {};
+        }
       }
-    });
+    ]);
 })();
